7ea9433436ef1ae2931bd6691b36f6417f92120d
nachiket mehta <nmehta@mediafly.com>
2015-04-22 13:55:27 -0500

    fix(VWR-986): use correct URL for second screen options dialog

--- src/mflyCommands.js
+++ src/mflyCommands.js
@@ -1,5 +1,5 @@
 /**
- * mflyCommands v1.4.8 | (c) 2013-2015, Mediafly, Inc.
+ * mflyCommands v1.4.9 | (c) 2013-2015, Mediafly, Inc.
  * mflyCommands is a singleton instance which wraps common mfly calls into a JavaScript object.
  * Before use, please be sure to call setPrefix if you are working on a development platform (e.g.
  * a local webserver on a PC) to override mfly:// with, for example, http://localhost:8000/ .
@@ -330,7 +330,7 @@ var mflyCommands = function () {
         },
 
         showSecondScreenOptions: function () {
-            doControlStatement(prefix + "control/showSecondScreenOptions");
+            doControlStatement(prefix + "control/secondScreenOptions");
         },
 
 

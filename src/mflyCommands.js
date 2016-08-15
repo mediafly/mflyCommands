9eab1d0b84e6431a32e97ebb92d888b8b343681d
Nachiket Mehta <nmehta@mediafly.com>
2016-08-15 14:02:06 -0500

    feature: postEvent command

--- src/mflyCommands.js
+++ src/mflyCommands.js
@@ -667,6 +667,14 @@ var mflyCommands = function () {
             });
         },
 
+        postEvent: function(key, properties) {
+            return $.Deferred(function (dfd) {
+                var params = 'events?key=' + key;
+                params += '&properties=' + properties;
+                _internalGetData(params, null, dfd, false);
+            });
+        },
+
         /**
         * Get raw data of Interactive via the embed function.
         * @param id Airship ID of the item to embed. Currently limited to images and other Interactives

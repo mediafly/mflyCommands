46e355817da0cbc7d7860b587b4d8ef08f4ea0b1
jcarroll-mediafly <jcarroll@mediafly.com>
2018-01-09 15:44:52 -0600

    Update copy.ts

--- commands/copy.ts
+++ commands/copy.ts
@@ -2,7 +2,7 @@ import { post } from './internalMethods'
 import { updateItemMetadata } from './updateMetadata'
 
 export default function copy(parentId, slug, name) {
-	const action = post(`items/copy`, { parentId, slug, name })
+	let action = post(`items/copy`, { parentId, slug, name })
 	
 	if (name) {
 		action = action.then((newItem) => updateItemMetadata(newItem.slug, {"title" : name}))

e504e5f6e79b984dfceb8363bac002c919ca36a8
Joe Carroll <jcarroll@mediafly.com>
2018-03-14 11:04:58 -0500

    generic async calls

--- commands/async.ts
+++ commands/async.ts
@@ -2,33 +2,46 @@ import { guid } from './utils'
 
 declare const InteractivesInterface: any
 
-export interface InteractivesInterfaceResponse {
-	data: any
-	status: any
+export interface AsyncCallbackResponse {
+    data: any
+    status: any
 }
 
 // dictionary guid -> anon funcions
 var callbacks = {}
 
 export function asyncCallback(guid, data) {
-	callbacks[guid](data)
-	callbacks[guid] = null
+    callbacks[guid](data)
+    callbacks[guid] = null
 }
 
 export function callAsync(verb, url, data = null) {
 
-	var newGuid = guid()
-	var deferred = $.Deferred()
+    var newGuid = guid()
+    var deferred = $.Deferred()
 
-	callbacks[newGuid] = (result: string) => {
-		deferred.resolve(result)
-	}
+    callbacks[newGuid] = (result: string) => {
+        deferred.resolve(result)
+    }
 
-	InteractivesInterface.handleAsync(newGuid, url, verb, JSON.stringify(data))
+    if (checkiOS() == true) {
+        var message = {'guid': newGuid, 'url': url, 'verb': verb, 'data': data}
+        window['webkit'].messageHandlers.HTMLtoiOS.postMessage(message);
+    } else { // Android
+        InteractivesInterface.handleAsync(newGuid, url, verb, JSON.stringify(data))
+    }
 
-	return deferred.promise()
+    return deferred.promise()
 }
 
-export function InteractivesInterfaceIsDefined() {
-	return typeof InteractivesInterface !== 'undefined'
+export function ShouldInterfaceViaAsyncCallbacks(verb) {
+    return checkAndroid(verb) || checkiOS()
+}
+
+function checkAndroid(verb) {
+    return typeof InteractivesInterface !== 'undefined' && verb != 'GET'
+}
+
+function checkiOS() {
+    return window['webkit'].messageHandlers !== 'undefined';
 }
--- commands/internalMethods.ts
+++ commands/internalMethods.ts
@@ -2,188 +2,201 @@ import device = require('./device')
 import { guid } from './utils'
 import { encode } from './Base64'
 import { isUnsupported } from './commandSupport'
-import { callAsync, InteractivesInterfaceIsDefined, InteractivesInterfaceResponse } from './async'
+import { callAsync, ShouldInterfaceViaAsyncCallbacks, AsyncCallbackResponse } from './async'
 
 declare const InteractivesInterface: any
 export function get(func, param = null, expectJson = true) {
-	var prefix = device.getPrefix()
-	var url = prefix + func + (param === null ? '' : '/' + param)
+    var prefix = device.getPrefix()
+    var url = prefix + func + (param === null ? '' : '/' + param)
 
-	if(isUnsupported(url)) {
-		throw new Error('This method is not supported on this platform.')
-	}
+    if(isUnsupported(url)) {
+        throw new Error('This method is not supported on this platform.')
+    }
 
-	var deferred = $.Deferred()
+    var deferred = $.Deferred()
 
-	$.ajax({
-		url: url,
-		success: function (data, textStatus, request) {
-			// Content retrieved. Transform to JSON if supposed to.
-			if (expectJson && request && request.getResponseHeader("Content-Type").indexOf("text/html") > -1) {
-				// This was sent back as text/html JSON.parse it to a JSON object.
-				data = JSON.parse(data)
-			}
+    if (ShouldInterfaceViaAsyncCallbacks('GET')) {
+        callAsync('GET', url, null)
+            .then((result: any) => {
 
-			// Resolve the promise.
-			deferred.resolveWith(this, [data, request.status])
-		},
-		error: function (data, status, request) {
-			// Content could not be retrieved. Reject the promise.
-			if (device.isWeb() && data.status === 401) {
-				// Viewer does not have an authenticated session. Take user to Viewer root.
-				sessionStorage.setItem('returnUrl', window.location.href)
-				window.location.replace(data.responseJSON.returnUrl)
-			}
+                const resultJSON : AsyncCallbackResponse = JSON.parse(result)
+                if(resultJSON.status < 300) {
+                    deferred.resolveWith(this, [resultJSON.data, resultJSON.status])
+                } else {
+                    deferred.rejectWith(this, [resultJSON.data, resultJSON.status])
+                }
+            })
+    } else {
+        $.ajax({
+            url: url,
+            success: function (data, textStatus, request) {
+                // Content retrieved. Transform to JSON if supposed to.
+                if (expectJson && request && request.getResponseHeader("Content-Type").indexOf("text/html") > -1) {
+                    // This was sent back as text/html JSON.parse it to a JSON object.
+                    data = JSON.parse(data)
+                }
 
-			deferred.reject(this, [request, data.status])
-		}
-	})
+                // Resolve the promise.
+                deferred.resolveWith(this, [data, request.status])
+            },
+            error: function (data, status, request) {
+                // Content could not be retrieved. Reject the promise.
+                if (device.isWeb() && data.status === 401) {
+                    // Viewer does not have an authenticated session. Take user to Viewer root.
+                    sessionStorage.setItem('returnUrl', window.location.href)
+                    window.location.replace(data.responseJSON.returnUrl)
+                }
 
-	return deferred.promise()
+                deferred.reject(this, [request, data.status])
+            }
+        })
+    }
+
+    return deferred.promise()
 }
 
 export function post(func: string, data?) {
-	var deferred = $.Deferred()
-	var prefix = device.getPrefix()
-	var url = prefix + func
+    var deferred = $.Deferred()
+    var prefix = device.getPrefix()
+    var url = prefix + func
 
-	if (isUnsupported(url)) {
-		throw new Error('This method is not supported on this platform.')
-	}
+    if (isUnsupported(url)) {
+        throw new Error('This method is not supported on this platform.')
+    }
 
-	if (InteractivesInterfaceIsDefined()) {
-		callAsync('POST', url, data)
-			.then((result: any) => {
+    if (ShouldInterfaceViaAsyncCallbacks('POST')) {
+        callAsync('POST', url, data)
+            .then((result: any) => {
 
-				const resultJSON : InteractivesInterfaceResponse = JSON.parse(result)
-				if(resultJSON.status < 300) {
-					deferred.resolveWith(this, [resultJSON.data, resultJSON.status])
-				} else {
-					deferred.rejectWith(this, [resultJSON.data, resultJSON.status])
-				}
-			})
-	} else {
-		$.ajax({
-			method: 'POST',
-			url,
-			data: JSON.stringify(data),
-			contentType: 'application/json; charset=utf-8',
-			success: function(data, textStatus, request) {
-				deferred.resolveWith(this, [data, request.status])
-			},
-			error: function (data, status, request) {
-				if (device.isWeb() && data.status === 401) {
-					// Viewer does not have an authenticated session. Take user to Viewer root.
-					sessionStorage.setItem('returnUrl', window.location.href)
-					window.location.replace(data.responseJSON.returnUrl)
-				}
+                const resultJSON : AsyncCallbackResponse = JSON.parse(result)
+                if(resultJSON.status < 300) {
+                    deferred.resolveWith(this, [resultJSON.data, resultJSON.status])
+                } else {
+                    deferred.rejectWith(this, [resultJSON.data, resultJSON.status])
+                }
+            })
+    } else {
+        $.ajax({
+            method: 'POST',
+            url,
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            success: function(data, textStatus, request) {
+                deferred.resolveWith(this, [data, request.status])
+            },
+            error: function (data, status, request) {
+                if (device.isWeb() && data.status === 401) {
+                    // Viewer does not have an authenticated session. Take user to Viewer root.
+                    sessionStorage.setItem('returnUrl', window.location.href)
+                    window.location.replace(data.responseJSON.returnUrl)
+                }
 
-				deferred.reject(this, [request, data.status])
-			}
-		})
-	}
+                deferred.reject(this, [request, data.status])
+            }
+        })
+    }
 
 
-	return deferred.promise()
+    return deferred.promise()
 }
 
 export function ddelete(func, data?) {
-	var deferred = $.Deferred()
-	var prefix = device.getPrefix()
-	var url = prefix + func
+    var deferred = $.Deferred()
+    var prefix = device.getPrefix()
+    var url = prefix + func
 
-	if (isUnsupported(url)) {
-		throw new Error('This method is not supported on this platform.')
-	}
+    if (isUnsupported(url)) {
+        throw new Error('This method is not supported on this platform.')
+    }
 
-	if (InteractivesInterfaceIsDefined()) {
-		
-		callAsync('DELETE', url, data)
-			.then((result: any) => {
-				const resultJSON : InteractivesInterfaceResponse = JSON.parse(result)
-				if(resultJSON.status < 300) {
-					deferred.resolveWith(this, [resultJSON.data, resultJSON.status])
-				} else {
-					deferred.rejectWith(this, [resultJSON.data, resultJSON.status])
-				}
-			})
-	} else {
-		$.ajax({
-			method: 'DELETE',
-			url,
-			data: JSON.stringify(data),
-			contentType: 'application/json; charset=utf-8',
-			success: function(data, textStatus, request) {
-				deferred.resolveWith(this, [data, request.status])
-			},
-			error: function (data, status, request) {
-				if (device.isWeb() && data.status === 401) {
-					// Viewer does not have an authenticated session. Take user to Viewer root.
-					sessionStorage.setItem('returnUrl', window.location.href)
-					window.location.replace(data.responseJSON.returnUrl)
-				}
+    if (ShouldInterfaceViaAsyncCallbacks('DELETE')) {
 
-				deferred.reject(this, [request, data.status])
-			}
-		})
-	}
+        callAsync('DELETE', url, data)
+            .then((result: any) => {
+                const resultJSON : AsyncCallbackResponse = JSON.parse(result)
+                if(resultJSON.status < 300) {
+                    deferred.resolveWith(this, [resultJSON.data, resultJSON.status])
+                } else {
+                    deferred.rejectWith(this, [resultJSON.data, resultJSON.status])
+                }
+            })
+    } else {
+        $.ajax({
+            method: 'DELETE',
+            url,
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            success: function(data, textStatus, request) {
+                deferred.resolveWith(this, [data, request.status])
+            },
+            error: function (data, status, request) {
+                if (device.isWeb() && data.status === 401) {
+                    // Viewer does not have an authenticated session. Take user to Viewer root.
+                    sessionStorage.setItem('returnUrl', window.location.href)
+                    window.location.replace(data.responseJSON.returnUrl)
+                }
+
+                deferred.reject(this, [request, data.status])
+            }
+        })
+    }
 
 
-	return deferred.promise()
+    return deferred.promise()
 }
 
 export function put(func, data = null) {
-	var deferred = $.Deferred()
-	var prefix = device.getPrefix()
-	var url = prefix + func
+    var deferred = $.Deferred()
+    var prefix = device.getPrefix()
+    var url = prefix + func
 
-	if (isUnsupported(url)) {
-		throw new Error('This method is not supported on this platform.')
-	}
+    if (isUnsupported(url)) {
+        throw new Error('This method is not supported on this platform.')
+    }
 
-	if (InteractivesInterfaceIsDefined()) {
-		callAsync('PUT', url, data)
-			.then((result: any) => {
-				const resultJSON : InteractivesInterfaceResponse = JSON.parse(result)
-				if(resultJSON.status < 300) {
-					deferred.resolveWith(this, [resultJSON.data, resultJSON.status])
-				} else {
-					deferred.rejectWith(this, [resultJSON.data, resultJSON.status])
-				}
-			})
-	} else {
-		$.ajax({
-			method: 'PUT',
-			data: JSON.stringify(data),
-			contentType: 'application/json; charset=utf-8',
-			url,
-			success: function(data, textStatus, request) {
-				deferred.resolveWith(this, [data, request.status])
-			},
-			error: function(data, status, request) {
-				if (device.isWeb() && data.status === 401) {
-					// Viewer does not have an authenticated session. Take user to Viewer root.
-					sessionStorage.setItem('returnUrl', window.location.href)
-					window.location.replace(data.responseJSON.returnUrl)
-				}
+    if (ShouldInterfaceViaAsyncCallbacks('PUT')) {
+        callAsync('PUT', url, data)
+            .then((result: any) => {
+                const resultJSON : AsyncCallbackResponse = JSON.parse(result)
+                if(resultJSON.status < 300) {
+                    deferred.resolveWith(this, [resultJSON.data, resultJSON.status])
+                } else {
+                    deferred.rejectWith(this, [resultJSON.data, resultJSON.status])
+                }
+            })
+    } else {
+        $.ajax({
+            method: 'PUT',
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            url,
+            success: function(data, textStatus, request) {
+                deferred.resolveWith(this, [data, request.status])
+            },
+            error: function(data, status, request) {
+                if (device.isWeb() && data.status === 401) {
+                    // Viewer does not have an authenticated session. Take user to Viewer root.
+                    sessionStorage.setItem('returnUrl', window.location.href)
+                    window.location.replace(data.responseJSON.returnUrl)
+                }
 
-				deferred.reject(this, [request, data.status])
-			}
-		})
-	}
+                deferred.reject(this, [request, data.status])
+            }
+        })
+    }
 
-	return deferred.promise()
+    return deferred.promise()
 }
 
 export function showUI(name, x, y, width, height) {
-	return post('control/show-ui', {
-		ui: name,
-		position: {
-			x,
-			y,
-			width,
-			height
-		}
-	})
+    return post('control/show-ui', {
+        ui: name,
+        position: {
+            x,
+            y,
+            width,
+            height
+        }
+    })
 }
 

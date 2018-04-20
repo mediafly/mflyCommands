607bee0070614c2a7c376a4e7d4eb39f239d1346
Nachiket Mehta <nmehta@mediafly.com>
2018-04-19 17:24:29 -0700

    expose mflyCommands as a global

--- src/mflyCommands.js
+++ src/mflyCommands.js
@@ -1122,6 +1122,7 @@ $.extend(mflyCommands, applicationSync);
 $.extend(mflyCommands, navigation);
 $.extend(mflyCommands, appFeatures);
 $.extend(mflyCommands, updateMetadata);
+window['mflyCommands'] = mflyCommands;
 module.exports = mflyCommands;
 
 },{"./commands/accountInfo":1,"./commands/appFeatures":2,"./commands/applicationSync":3,"./commands/async":4,"./commands/collections":5,"./commands/controls":7,"./commands/copy":8,"./commands/credentials":9,"./commands/device":10,"./commands/downloader":11,"./commands/email":12,"./commands/embed":13,"./commands/filter":14,"./commands/folder":15,"./commands/gpsCoordinates":16,"./commands/interactiveInfo":17,"./commands/item":19,"./commands/localKeyValueStorage":20,"./commands/navigation":21,"./commands/notification":22,"./commands/onlineStatus":23,"./commands/openWindow":24,"./commands/postAction":25,"./commands/postEvent":26,"./commands/search":27,"./commands/syncedKeyValueStorage":28,"./commands/systemInfo":29,"./commands/updateMetadata":30,"./commands/uploadUrl":31,"./commands/version":33}],35:[function(_dereq_,module,exports){

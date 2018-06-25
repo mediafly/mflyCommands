5d850cc487930f48e266003840900b972230db52
Nachiket Mehta <nmehta@mediafly.com>
2018-06-25 12:17:57 -0700

    Provide context about how the extension was invoked

--- commands/interactiveInfo.ts
+++ commands/interactiveInfo.ts
@@ -2,5 +2,28 @@ import {getDeviceType, deviceTypes} from './device'
 import { get } from './internalMethods'
 
 export default function getInteractiveInfo () {
+
+	const interactiveContext = JSON.parse(sessionStorage['viewerInteractiveContext'])
 	return get('interactive')
+		.then(info => {
+			if (!!interactiveContext) {
+
+				switch (interactiveContext.type) {
+					case 'collection':
+						info.invokedFrom = 'collection'
+						info.invokedFromSlug = interactiveContext.id
+						return info
+
+					case 'folder':
+						info.invokedFrom = 'folder'
+						info.invokedFromSlug = interactiveContext.parentSlug
+						return info
+				
+					default:
+						return info
+				}
+			}
+
+			return info
+		})
 }

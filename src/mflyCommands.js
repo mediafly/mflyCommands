dfcb55a7c663fb3c9d3022bade756192b9abfaab
Nachiket Mehta <nmehta@mediafly.com>
2018-06-26 08:27:11 -0700

    build js

--- src/mflyCommands.js
+++ src/mflyCommands.js
@@ -431,11 +431,11 @@ function getInteractiveInfo() {
             switch (interactiveContext.type) {
                 case 'collection':
                     info.invokedFrom = 'collection';
-                    info.invokedFromSlug = interactiveContext.id;
+                    info.invokedFromId = interactiveContext.id;
                     return info;
                 case 'folder':
                     info.invokedFrom = 'folder';
-                    info.invokedFromSlug = interactiveContext.parentSlug;
+                    info.invokedFromId = interactiveContext.parentSlug;
                     return info;
                 default:
                     return info;
@@ -1146,7 +1146,7 @@ module.exports = mflyCommands;
 },{"./commands/accountInfo":1,"./commands/appFeatures":2,"./commands/applicationSync":3,"./commands/async":4,"./commands/collections":5,"./commands/controls":7,"./commands/copy":8,"./commands/credentials":9,"./commands/device":10,"./commands/downloader":11,"./commands/email":12,"./commands/embed":13,"./commands/filter":14,"./commands/folder":15,"./commands/gpsCoordinates":16,"./commands/interactiveInfo":17,"./commands/item":19,"./commands/localKeyValueStorage":20,"./commands/navigation":21,"./commands/notification":22,"./commands/onlineStatus":23,"./commands/openWindow":24,"./commands/postAction":25,"./commands/postEvent":26,"./commands/search":27,"./commands/syncedKeyValueStorage":28,"./commands/systemInfo":29,"./commands/updateMetadata":30,"./commands/uploadUrl":31,"./commands/version":33}],35:[function(_dereq_,module,exports){
 module.exports={
   "name": "mfly-commands",
-  "version": "3.1.2",
+  "version": "3.2.0",
   "description": "mflyCommands.js for Mediafly Interactives",
   "main": "src/mflyCommands.js",
   "scripts": {
